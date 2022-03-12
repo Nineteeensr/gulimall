dbf3acc7b97e36bfc34974f7105cb9e42d3ba93e
cuiyuangeng <1021716024@qq.com>
2022-03-12 12:41:12 +0800

    项目初始结构创建

---
+++ gulimall-member/src/test/java/com/cyg/gulimall/member/GulimallMemberApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cyg.gulimall.member;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GulimallMemberApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
