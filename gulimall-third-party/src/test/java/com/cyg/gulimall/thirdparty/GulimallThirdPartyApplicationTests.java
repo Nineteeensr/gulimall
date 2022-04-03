229847097cd40f1777e8c6725774b81bd97cf859
cuiyuangeng <1021716024@qq.com>
2022-04-03 20:51:51 +0800

    第三方服务模块搭建

---
+++ gulimall-third-party/src/test/java/com/cyg/gulimall/thirdparty/GulimallThirdPartyApplicationTests.java
@@ -0,0 +1,50 @@
+package com.cyg.gulimall.thirdparty;
+
+import com.aliyun.oss.ClientException;
+import com.aliyun.oss.OSSClient;
+import com.aliyun.oss.OSSException;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+
+@SpringBootTest
+class GulimallThirdPartyApplicationTests {
+    @Autowired
+    OSSClient ossClient;
+    @Test
+    void contextLoads() throws FileNotFoundException {
+        // 填写Bucket名称，例如examplebucket。
+        String bucketName = "gulimall-cyg";
+        // 填写Object完整路径，完整路径中不能包含Bucket名称，例如exampledir/exampleobject.txt。
+        String objectName = "huawei.png";
+        // 填写本地文件的完整路径，例如D:\\localpath\\examplefile.txt。
+        // 如果未指定本地路径，则默认从示例程序所属项目对应本地路径中上传文件流。
+        String filePath= "E:\\谷粒商城\\01-分布式基础篇\\商品图片素材\\huawei.png";
+        try {
+            InputStream inputStream = new FileInputStream(filePath);
+            // 创建PutObject请求。
+            ossClient.putObject(bucketName, objectName, inputStream);
+        } catch (OSSException oe) {
+            System.out.println("Caught an OSSException, which means your request made it to OSS, "
+                    + "but was rejected with an error response for some reason.");
+            System.out.println("Error Message:" + oe.getErrorMessage());
+            System.out.println("Error Code:" + oe.getErrorCode());
+            System.out.println("Request ID:" + oe.getRequestId());
+            System.out.println("Host ID:" + oe.getHostId());
+        } catch (ClientException ce) {
+            System.out.println("Caught an ClientException, which means the client encountered "
+                    + "a serious internal problem while trying to communicate with OSS, "
+                    + "such as not being able to access the network.");
+            System.out.println("Error Message:" + ce.getMessage());
+        } finally {
+            if (ossClient != null) {
+                ossClient.shutdown();
+            }
+        }
+    }
+
+}
