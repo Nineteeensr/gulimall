2bb86b09f61f97a1976354cec9a1511218eeb60a
cuiyuangeng <1021716024@qq.com>
2022-04-05 18:50:51 +0800

    添加：商品模块-平台属性-规格参数列表VO类

---
+++ gulimall-product/src/main/java/com/cyg/gulimall/product/vo/AttrRespVo.java
@@ -0,0 +1,19 @@
+package com.cyg.gulimall.product.vo;
+
+import lombok.Data;
+
+/**
+ * @author CuiYuangeng
+ * @create 2022-04-05 17:13
+ */
+@Data
+public class AttrRespVo extends AttrVo{
+    /**
+     * 所属分类名
+     */
+   private String catelogName;
+    /**
+     * 所属主体名
+     */
+   private String groupName;
+}
