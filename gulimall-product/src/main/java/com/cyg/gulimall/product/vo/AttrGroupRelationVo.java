518adb5a48b90b2718a834718e15ed9f0977613e
cuiyuangeng <1021716024@qq.com>
2022-04-06 22:55:48 +0800

    添加：AttrGroupRelationVo类

---
+++ gulimall-product/src/main/java/com/cyg/gulimall/product/vo/AttrGroupRelationVo.java
@@ -0,0 +1,15 @@
+package com.cyg.gulimall.product.vo;
+
+import lombok.Data;
+
+/**
+ * @author CuiYuangeng
+ * @create 2022-04-06 16:23
+ */
+@Data
+public class AttrGroupRelationVo {
+
+    private Long attrId;
+
+    private Long attrGroupId;
+}
